33afa5ba921658c0e5889bac8ddde08b730e1799
Arthur Baudoin <arthur.baudoin@epfedu.fr>
2022-10-30 23:36:57 +0100

    création de tout les méthode de convertion plus constructeur pour le nb de convertion plus override

--- TP2_convertisseurObjet/src/tp2_convertisseurobjet/Convertisseur.java
+++ TP2_convertisseurObjet/src/tp2_convertisseurobjet/Convertisseur.java
@@ -9,5 +9,51 @@ package tp2_convertisseurobjet;
  * @author arthu
  */
 public class Convertisseur {
-    
+ int nbConversions;
+ 
+ public Convertisseur () {              //constructeur permettant de compter les conversions
+ nbConversions = 0 ;
 }
+
+ public double CelciusVersKelvin(double tempCelcius) {       // méthode de convertion de celcius en kelvin 
+        nbConversions +=1;                                      // augmente le nb de conversions de 1
+        return tempCelcius + 273.15;                            // renvoie la température une fois convertie
+    }
+ public double KelvinVersCelcius(double tempKelvin) {        // méthode de convertion de Kelvin en Celcius 
+        nbConversions +=1;
+        return tempKelvin - 273.15;
+    }
+    public double FarenheitVersCelcius(double tempFarenheit) {  // analogue...
+        nbConversions +=1;
+        return (tempFarenheit - 32.0)/1.8;
+    }
+    public double CelciusVersFarenheit(double tempCelcius) {        
+        nbConversions +=1;
+        return (tempCelcius *1.8 + 32);
+    }
+    public double FarenheitVersKelvin(double tempFarenheit) {
+        nbConversions += 1; 
+        return (tempFarenheit-32)/1.8 + 273.15;
+    }
+    public double KelvinVersFarenheit(double tempKelvin) {
+        nbConversions += 1; 
+        return (tempKelvin-273.15)*1.8 + 32;
+    }
+    @Override
+    public String toString () {
+    return "nb de conversions: "+ nbConversions;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+    
