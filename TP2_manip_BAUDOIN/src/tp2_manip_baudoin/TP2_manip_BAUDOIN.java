dd511a831ff75542d9448e57f387570f1822d8af
Arthur Baudoin <arthur.baudoin@epfedu.fr>
2022-10-31 20:32:38 +0100

    Réalisation de la boucle ainsi que l'affichage en sortie

--- TP2_manip_BAUDOIN/src/tp2_manip_baudoin/TP2_manip_BAUDOIN.java
+++ TP2_manip_BAUDOIN/src/tp2_manip_baudoin/TP2_manip_BAUDOIN.java
@@ -37,12 +37,13 @@ public class TP2_manip_BAUDOIN {
         assiette1 = assiette3; // Etant donner que 2 et 3 désigne le même objet
         // 1 et 2 ont donc été intervertie
         
-        
+         Moussaka [] Tab = new Moussaka[10]; //création du tableau
     
-                               
-        
-        
-        
+    for (int i=0 ; i<10 ; i++){ //Création d'une boucle 10 fois
+        Tab[i] = new Moussaka(550+i); // apparition d'une mousaka a chaque fois avec 1 ajouté 
+        //a chaque ittération 
+    }
+    System.out.println("calories final du plat:" +Tab[5].nbcalories);
     }
     
 }
