664040e6551367d39b1a6c807376e715adfcb528
Arthur Baudoin <arthur.baudoin@epfedu.fr>
2022-11-14 11:12:05 +0100

    Réponse partie 1.3: - création dans le main des différentes épées et batons - création du tableau dynamique ajout avec la commande add - création d'une boucle qui parcours le tableau et affiche les attributs des objets

--- TP3_BAUDOIN/src/tp3_baudoin/TP3_BAUDOIN.java
+++ TP3_BAUDOIN/src/tp3_baudoin/TP3_BAUDOIN.java
@@ -4,6 +4,8 @@
  */
 package tp3_baudoin;
 
+import java.util.Vector;
+
 /**
  *
  * @author arthu
@@ -14,7 +16,21 @@ public class TP3_BAUDOIN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Arme Sword1 = new Epee("Excalibur", 7.5);
+        Arme Sword2 = new Epee("Durandal", 4.7);
+        Arme Stick1 = new Baton("Chêne", 4.5);
+        Arme Stick2 = new Baton("Charme", 5.6);
+
+        Vector tab_dyn = new Vector();
+        tab_dyn.add(Sword1);
+        tab_dyn.add(Sword2);
+        tab_dyn.add(Stick1);
+        tab_dyn.add(Stick2);
+
+        for (int i = 0; i < tab_dyn.size(); i++) {
+            System.out.println(tab_dyn.elementAt(i));
+        }
+
     }
-    
 }
