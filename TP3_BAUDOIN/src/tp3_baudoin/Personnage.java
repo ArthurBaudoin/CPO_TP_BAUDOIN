f4bfd27f6ab5352c8ef6555261a7df3ba850797c
Arthur Baudoin <arthur.baudoin@epfedu.fr>
2022-11-14 11:41:37 +0100

    Réponse partie 2.1 : - création calsses magicien, gueurier, personnage - constructeur des différentes classes - création toString pour connaitre le niveau de vie - on fait de même pour le nom

---
+++ TP3_BAUDOIN/src/tp3_baudoin/Personnage.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_baudoin;
+
+/**
+ *
+ * @author arthu
+ */
+public class Personnage {
+    String nom;
+    double niv_vie;
+    
+public Personnage (String nom, double niv_vie){
+    this.niv_vie = niv_vie;
+    this.nom = nom;
+}
+
+public double connaitre_niv_vie (){
+    return niv_vie;
+}
+
+@Override
+public String toString(){
+    return "nom personnage:" + nom + " niveau de vie:" + niv_vie;
+    
+}
+
+public String connaitre_nom (){
+    return nom;
+}
+
+}
