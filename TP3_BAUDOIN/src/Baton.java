4645429af674f8815ce84cad75380528677fe1f0
Arthur Baudoin <arthur.baudoin@epfedu.fr>
2022-11-14 10:22:28 +0100

    Réponse partie 1.2 : - création des deux classes enfants épée et baton - création du constructeur de épée et baton - chacun des paramètres finesse et age sont relié au niveau de l'arme de même pour les noms

---
+++ TP3_BAUDOIN/src/Baton.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arthu
+ */
+public class Baton extends Arme {
+    
+
+    public Baton (String blaze,int age){
+        niveau_attaque = age;
+        nom = blaze;
+    
+    }
+}
